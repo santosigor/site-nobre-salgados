2b2531acd2ba3700907d59046a3ac67ee4e723a4
Igor dos Santos <igor.santos@fiap.com.br>
2018-06-27 14:33:26 -0300

    dev

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,17 @@ $(document).ready(function() {
 	  $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 800, 'linear');
 	});
 
+	$('.catalog-item').on('click', function(e) {
+	  e.preventDefault();
+	  var prod = $(this).attr('data-produto');
+	  $('.modal-catalog-item#'+prod).addClass('active');
+	  $('body').css('overflow-y','hidden');
+	});
+
+	$('.close-modal-catalog-item').on('click', function(e) {
+	  e.preventDefault();
+	  $('body').css('overflow-y','auto');
+	  $('.modal-catalog-item').removeClass('active');
+	});
+
 });
